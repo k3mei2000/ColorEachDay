96c4aa7a5be23ab79ed1cc09bd095f4cee44658e
Kent Mei <k3mei2000@gmail.com>
2020-06-09 21:19:21 -0400

    Initial Commit

---
+++ ColorEachDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorEachDay
+//
+//  Created by Kent Mei on 6/9/20.
+//  Copyright © 2020 Kent Mei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
