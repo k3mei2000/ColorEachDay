9b4d4426276aaf3f76fc542d23a3728506072c7f
Kent Mei <k3mei2000@gmail.com>
2020-06-24 20:57:05 -0400

    Create layout constraints for color picker and brightness slider and change new color view background color upon value changed.

--- ColorEachDay/ColorPickerViewController.swift
+++ ColorEachDay/ColorPickerViewController.swift
@@ -14,23 +14,57 @@ class ColorPickerViewController: UIViewController {
     
     @IBOutlet var colorPickerView: UIView!
     @IBOutlet var brightnessSliderView: UIView!
+    @IBOutlet var previousColorView: UIView!
+    @IBOutlet var newColorView: UIView!
+    
+    var colorPicker: ChromaColorPicker!
+    var brightnessSlider: ChromaBrightnessSlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupColorPicker()
         
+        
     }
     
     func setupColorPicker() {
-        let colorPicker = ChromaColorPicker(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
-        colorPickerView.addSubview(colorPicker)
         
-        let brightnessSlider = ChromaBrightnessSlider(frame: CGRect(x: 0, y: 0, width: 280, height: 32))
+        colorPicker = ChromaColorPicker(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        
+        colorPickerView.addSubview(colorPicker)
+        colorPicker.translatesAutoresizingMaskIntoConstraints = false;
+        colorPicker.heightAnchor.constraint(equalToConstant: CGFloat(300.0)).isActive = true;
+        colorPicker.widthAnchor.constraint(equalToConstant: CGFloat(300.0)).isActive = true;
+        colorPicker.centerXAnchor.constraint(equalTo: colorPickerView.centerXAnchor).isActive = true;
+        colorPicker.centerYAnchor.constraint(equalTo: colorPickerView.centerYAnchor).isActive = true;
+        
+        colorPicker.addTarget(self, action: #selector(updateColor(_:)), for: .valueChanged)
+        
+        
+        
+        brightnessSlider = ChromaBrightnessSlider(frame: CGRect(x: 0, y: 0, width: 280, height: 32))
+        
         brightnessSliderView.addSubview(brightnessSlider)
+        brightnessSlider.translatesAutoresizingMaskIntoConstraints = false;
+        brightnessSlider.heightAnchor.constraint(equalToConstant: CGFloat(32.0)).isActive = true;
+        brightnessSlider.widthAnchor.constraint(equalToConstant: CGFloat(280.0)).isActive = true;
+        brightnessSlider.centerXAnchor.constraint(equalTo: brightnessSliderView.centerXAnchor).isActive = true;
+        brightnessSlider.centerYAnchor.constraint(equalTo: brightnessSliderView.centerYAnchor).isActive = true;
+        brightnessSlider.addTarget(self, action: #selector(updateBrightness(_:)), for: .valueChanged)
+        
         
         colorPicker.connect(brightnessSlider)
         
         colorPicker.addHandle()
     }
+    
+    @objc func updateColor(_ colorPicker: ChromaColorPicker) {
+        newColorView.backgroundColor = colorPicker.currentHandle?.color
+    }
+    
+    @objc func updateBrightness(_ brightnessSlider: ChromaBrightnessSlider) {
+        newColorView.backgroundColor = brightnessSlider.currentColor
+    }
+    
 }
